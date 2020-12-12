f057048a462e218c3a715898e1a7791690e000de
ouafais <56456624+ouafaa1982@users.noreply.github.com>
2020-12-12 22:34:06 +0100

    added the css grid to the keybord clavier

--- src/component/Button.js
+++ src/component/Button.js
@@ -3,11 +3,45 @@ import React from 'react';
 
 import './Button.css'
 
-function Button (){
-
-    return (
-    <button> button </button>
-    )
+function Button (props){
+    let typeboutton=props.typeBT;
+    console.log(typeboutton);
+    
+    if (typeboutton==='operateur')  
+    {
+        return(   
+            <span className="operateurKeys">
+                <button className="operateur Keybord" id="clear">C</button>
+                <button className="operateur Keybord" id="retour">CE</button>
+                <button className="operateur Keybord" id="%">%</button>
+                <button className="operateur Keybord" id="/">&#247;</button>
+                <button className="operateur Keybord" id="*">&times;</button>
+                <button className="operateur Keybord" id="-">-</button>
+                <button className="operateur Keybord" id="+">+</button>
+                <button className="operateur Keybord" id="=">=</button>
+            </span> 
+         )      
+    }  
+    else{
+        return(  
+             <span className="nombreKeys">
+                <button className="nombre Keybord" id="1">1</button>
+               <button className="nombre Keybord" id="2">2</button>
+                <button className="nombre Keybord" id="3">3</button>
+                <button className="nombre Keybord" id="4">4</button>
+                <button className="nombre Keybord" id="5">5</button>
+                <button className="nombre Keybord" id="6">6</button>
+                <button className="nombre Keybord" id="7">7</button>
+                <button className="nombre Keybord" id="8">8</button>
+                <button className="nombre Keybord" id="9">9</button>
+                <button className="nombre Keybord" id="0">0</button>
+                <button className="nombre Keybord" id=".">.</button>
+                
+            </span> 
+         )      
+    }
+    
+    
         
     
 }
